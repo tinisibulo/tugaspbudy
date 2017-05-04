32950375fc57135cedd32a462a23cd65a19c7359
tinisibulo <sibulotini@gmail.com>
2017-05-04 13:07:22 +0700

    Add files via upload

---
+++ tini.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(VOID)
+{
+STARTUPINFO si;
+PROCESS_INFORMASTION pi;
+
+	/*allocate memory */
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	/* create child process */	
+	if (!CreateProcess(NULL, /* use command line */
+	   "C:\\WINDOWS\\system32\\mspaint.exe", /* command */
+	   NULL, /* don't inherit process handle */
+	   NULL, /* don't inherit process handle */
+     	   FALSE, /* disable handle inheritance*/
+  	   0, /* no creation flags */
+	   NULL, /* use parent's envirotment block */
+	   NULL, /* use parent's existing directory */
+	   &si,
+	   &pi))
+	{
+	   fprintf(stderr, "Create Process Failed");
+	   return -1;
+	}
+	
+	/* parent will wait for the child to complete */
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	printf("Child Complete");
+        printf("Nama : Mahrus Khomaini");
+	printf("NIM : 14201075");
+
+	/* close handles*/
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+}
